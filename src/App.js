4e6a831d920d3672ce5f103b2c2254e3f7cf3046
Blue Lindsey <linzblue5@gmail.com>
2020-03-27 10:41:49 -0500

    Refatored into cards with React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import UserCard from './components/UserCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Card from 'react-bootstrap/Card'
 
 function App() {
   return (
     <div className="App">
-    <h1>User Info</h1>
-    <UserCard />
-    <div className="row"><UserCard />
-    </div>
+      <div className="container">
+        <h1>User Info</h1>
+
+        <UserCard />
+        <div className="row">
+          <UserCard />
+        </div>
+      </div>
     </div>
   );
 }
